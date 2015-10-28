3e181a17aab61d9b3fd3f69b987d90265caa8920
Nature Song <sol2man2@gmail.com>
2015-10-28 10:38:51 +0900

    init

---
+++ src/Ch01.hs
@@ -0,0 +1,15 @@
+
+
+
+-- Exercises
+-- 1. Give another possible calculation
+ -- for the result of double ( double 2).
+-- 2. Show that sum [ x ] = x for any number x .
+-- 3. Define a function product that produces the product of a list of numbers,
+-- and show using your definition that product [2, 3, 4] = 24.
+-- 4. How should the definition of the function qsort be modified so that it
+-- produces a reverse sorted version of a list?
+-- 5. What would be the effect of replacing ≤ by < in the definition of qsort ?
+-- Hint: consider the example qsort [2, 2, 3, 1, 1].
+
+
